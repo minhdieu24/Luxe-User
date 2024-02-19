2d7d104e902ef7486eb2201268411e67e56fed68
Minh Dieu <minhdieu2003@gmail.com>
2024-02-19 17:11:34 +0700

    Navigation

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Click hambergur menu
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const overlay = document.querySelector(".menu-overlay");
+const menuDrawer = document.querySelector(".menu-drawer");
+
+hamburgerMenu.addEventListener("click", () => {
+  // overlay.classList.toggle("active");
+  // menuDrawer.classList.toggle("active");
+  document.write("Bạn đã click vào nút!");
+});
