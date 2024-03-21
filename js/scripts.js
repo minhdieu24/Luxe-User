f9cde4a40da1a64f7c5871e55b2e3237f21fd47a
Thuhien021003 <nguyenluongthuhienqn1@gmail.com>
2024-03-21 20:18:11 +0700

    6

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,24 @@ function Increase(){
   count++;
   document.getElementById("count").innerHTML = count;
 }
+
+function darkFunction() {
+  const element = document.body;
+  element.classList.toggle("dark-mode");
+  // Đổi ảnh marquee chỉ khi bật dark-mode
+  const imageMarquee = document.querySelector(".mar img");
+  //Đổi màu shadow của imgWrap khi qua dark mode
+  const imgWrapShadows = document.querySelectorAll(".product-card");
+  if (element.classList.contains("dark-mode")) {
+    imageMarquee.src = "./img/marquee-dark.png";
+    imgWrapShadows.forEach((element) => {
+      element.style.boxShadow = "0px 20px 60px 0px rgba(0, 0, 0, 0.2)";
+    });
+  } else {
+    imageMarquee.src = "./img/marquee-light.png";
+    imgWrapShadows.forEach((element) => {
+      element.style.boxShadow = "0px 60px 20px 0px rgba(237, 237, 246, 0.20)";
+    });
+  }
+}
+
