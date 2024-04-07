871d94ce17f5ae6edcbee335525d9c28e69dc872
yenvy031110 <141745084+yenvy031110@users.noreply.github.com>
2024-04-07 14:34:54 +0700

    tt

--- js/scripts.js
+++ js/scripts.js
@@ -19,13 +19,41 @@ function burgerFunction() {
   function darkFunction() {
     const element = document.body;
     element.classList.toggle("dark-mode");
-   //Đổi ảnh icon xóa
+   //Đổi ảnh icon xóa //chưa chạy được
    const imageDelete = document.querySelector(".delete img");
    if (element.classList.contains("dark-mode")) {
     imageDelete.src = "./icon/delete-dark.svg";
   } else {
     imageDelete.src = "./icon/delete.svg";
   }
+}
+// Lấy modal
+var modal = document.getElementById("cartModal");
+
+// Lấy nút đóng
+var closeBtn = modal.querySelector(".close");
+
+// Khi người dùng nhấn nút đóng, ẩn modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Hiển thị modal khi sản phẩm được thêm vào giỏ hàng
+function showCartModal() {
+  modal.style.display = "block";
+  
+  // Tự động ẩn modal sau 3 giây
+  setTimeout(function() {
+    modal.style.display = "none";
+  }, 3000);
+}
+
+// Sự kiện click cho nút "Thêm vào giỏ hàng"
+function addToCart() {
+  // Thêm sản phẩm vào giỏ hàng (có thể là một chức năng PHP)
+  
+  // Hiển thị modal thông báo
+  showCartModal();
 }
   
       
