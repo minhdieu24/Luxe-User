10b33acb54b0c48b661c101ff7ce52450ed85765
kimleeda <142829478+kimleeda@users.noreply.github.com>
2024-04-10 12:48:56 +0700

    kim

---
+++ js/danhmuc.js
@@ -0,0 +1,40 @@
+function darkFunction() {
+  var elmt = document.body;
+  elmt.classList.toggle("dark-mode");
+}
+// Click hambergur menu
+function burgerFunction() {
+  const overlay = document.querySelector(".menu-overlay");
+  const menuDrawer = document.querySelector(".menu-drawer");
+  overlay.classList.toggle("active");
+  menuDrawer.classList.toggle("active");
+}
+//Load slider
+function loadImage() {
+  const imgPosition = document.querySelectorAll(".hero-slider img");
+  const imgContainer = document.querySelector(".hero-slider");
+  const dotItem = document.querySelectorAll(".dot");
+  let imgNumber = imgPosition.length;
+  let index = 0;
+  imgPosition.forEach(function (image, index) {
+    image.style.left = index * 100 + "%";
+    dotItem[index].addEventListener("click", function () {
+      slide(index);
+    });
+  });
+  function imgSlide() {
+    index++;
+    if (index >= imgNumber) {
+      index = 0;
+    }
+    slide(index);
+  }
+
+  function slide(index) {
+    imgContainer.style.left = index * -100 + "%";
+    const dotActive = document.querySelector(".dot-container .dot.active");
+    dotActive.classList.remove("active");
+    dotItem[index].classList.add("active");
+  }
+  setInterval(imgSlide, 5000);
+}
