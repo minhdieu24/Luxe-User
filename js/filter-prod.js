98a2ae8f54fcb9829828683abd1a8635be8ff4de
kimleeda <142829478+kimleeda@users.noreply.github.com>
2024-04-10 10:57:02 +0700

    kim

--- js/filter-prod.js
+++ js/filter-prod.js
@@ -1,63 +1,63 @@
-// <!-- bao gồm thư viện jQuery -->
-src =
-  "https://code.jquery.com/jquery-3.6.0.min.js" >
-  $(document).ready(function () {
-    $(".custom-filter-input input").click(function () {
-      // lấy giá trị của thuộc tính data-value của ô lựa chọn đã được nhấp vào và lưu trữ vào biến value
-      var value = $(this).attr("data-value");
-      console.log(value);
-      $(
-        //hiển thị giá trị đã chọn trong bộ lọc
-        ".header_filter_chosen .filter_chosen[data-value=" + value + "]"
-      ).toggleClass("show_tag");
+// // <!-- bao gồm thư viện jQuery -->
+// src =
+//   "https://code.jquery.com/jquery-3.6.0.min.js" >
+//   $(document).ready(function () {
+//     $(".custom-filter-input input").click(function () {
+//       // lấy giá trị của thuộc tính data-value của ô lựa chọn đã được nhấp vào và lưu trữ vào biến value
+//       var value = $(this).attr("data-value");
+//       console.log(value);
+//       $(
+//         //hiển thị giá trị đã chọn trong bộ lọc
+//         ".header_filter_chosen .filter_chosen[data-value=" + value + "]"
+//       ).toggleClass("show_tag");
 
-      var check_filter = false;
-      $(".filter_chosen").each(function () {
-        if ($(this).hasClass("show_tag")) {
-          check_filter = true;
-        }
-      });
-      if (check_filter) {
-        $(".clear_all").addClass("show_btn");
-      } else {
-        $(".clear_all").removeClass("show_btn");
-      }
-    });
+//       var check_filter = false;
+//       $(".filter_chosen").each(function () {
+//         if ($(this).hasClass("show_tag")) {
+//           check_filter = true;
+//         }
+//       });
+//       if (check_filter) {
+//         $(".clear_all").addClass("show_btn");
+//       } else {
+//         $(".clear_all").removeClass("show_btn");
+//       }
+//     });
 
-    $(".clear_all a").click(function () {
-      $(".filter_chosen.show_tag a").trigger("click");
-    });
+//     $(".clear_all a").click(function () {
+//       $(".filter_chosen.show_tag a").trigger("click");
+//     });
 
-    $(".filter-color label").click(function () {
-      $(this).toggleClass("active");
-    });
+//     $(".filter-color label").click(function () {
+//       $(this).toggleClass("active");
+//     });
 
-    $(".header_filter_chosen .filter_chosen > a").click(function () {
-      var $parent = $(this).parent();
-      //      $(this).parent('.filter_chosen').removeClass('show_tag');
+//     $(".header_filter_chosen .filter_chosen > a").click(function () {
+//       var $parent = $(this).parent();
+//       //      $(this).parent('.filter_chosen').removeClass('show_tag');
 
-      var val = $parent.attr("data-value");
-      $(".custom-filter-input[data-filter1=" + val + "]")
-        .find("input")
-        .trigger("click");
-    });
-    $(".header_filter_chosen .filter_chosen.price > a").click(function () {
-      var $parent = $(this).parent();
-      var val = $parent.attr("data-value");
-      $(".custom-filter-input[data-filter1=0-max]")
-        .find("input")
-        .trigger("click");
-    });
-  });
+//       var val = $parent.attr("data-value");
+//       $(".custom-filter-input[data-filter1=" + val + "]")
+//         .find("input")
+//         .trigger("click");
+//     });
+//     $(".header_filter_chosen .filter_chosen.price > a").click(function () {
+//       var $parent = $(this).parent();
+//       var val = $parent.attr("data-value");
+//       $(".custom-filter-input[data-filter1=0-max]")
+//         .find("input")
+//         .trigger("click");
+//     });
+//   });
 
-$(document).ready(function () {
-  $(".filter-custom > span").click(function () {
-    $(".groupFilterNew").toggle("fast");
-  });
-  $(".btn-custom-filter").click(function () {
-    $(".groupFilterNew").hide();
-  });
-  $(".cancel-filter").click(function () {
-    $(".clear_all > a").trigger("click");
-  });
-});
+// $(document).ready(function () {
+//   $(".filter-custom > span").click(function () {
+//     $(".groupFilterNew").toggle("fast");
+//   });
+//   $(".btn-custom-filter").click(function () {
+//     $(".groupFilterNew").hide();
+//   });
+//   $(".cancel-filter").click(function () {
+//     $(".clear_all > a").trigger("click");
+//   });
+// });
--- js/pagination.js
+++ js/pagination.js
@@ -1,83 +1,83 @@
-//pagination sản phẩm
-// Lựa chọn các phần tử DOM
-const pagination_startBtn = document.querySelector("#pagination_startBtn"),
-  pagination_endBtn = document.querySelector("#pagination_endBtn"),
-  prevNext = document.querySelectorAll(".prevNext"),
-  numbers = document.querySelectorAll(".link");
+// //pagination sản phẩm
+// // Lựa chọn các phần tử DOM
+// const pagination_startBtn = document.querySelector("#pagination_startBtn"), // nút "Bắt đầu"
+//   pagination_endBtn = document.querySelector("#pagination_endBtn"), // nút "Kết thúc"
+//   prevNext = document.querySelectorAll(".prevNext"), // mảng chứa nút "Previous" và "Next"
+//   numbers = document.querySelectorAll(".link"); // mảng chứa các liên kết số trang
 
-// Thiết lập bước khởi đầu
-let currentStep = 0; //Biến currentStep được sử dụng để lưu trạng thái hiện tại của bước.
+// // Thiết lập bước khởi đầu
+// let currentStep = 0; //Biến currentStep được sử dụng để lưu trạng thái hiện tại của bước.
 
-//Hàm cập nhật trạng thái nút
-const updateBtn = () => {
-  // Nếu ở bước cuối tức là trang cuối
-  if (currentStep === 3) {
-    pagination_endBtn.disabled = true; //nút "End" sẽ bị vô hiệu hóa
-    prevNext[1].disabled = true; //nút "Next" bị vô hiệu hóa
-  }
-  //Nếu currentStep bằng 0, hoặc đang ở trang đầu tiên
-  else if (currentStep === 0) {
-    // Nếu ở trang đầu tiên
-    pagination_startBtn.disabled = true; //nút "Start" sẽ bị vô hiệu hóa
-    prevNext[0].disabled = true; // nút "Previous" bị vô hiệu hóa
-  }
-  //Nếu currentStep không phải là 0 hoặc 3
-  /*tất cả các nút "Start", "End", "Previous" và "Next" đều được kích hoạt để cho phép người dùng di chuyển qua các trang khác nhau. */
-  else {
-    pagination_endBtn.disabled = false;
-    prevNext[1].disabled = false;
-    pagination_startBtn.disabled = false;
-    prevNext[0].disabled = false;
-  }
-};
+// //Hàm cập nhật trạng thái nút
+// const updateBtn = () => {
+//   // Nếu ở bước cuối tức là trang cuối
+//   if (currentStep === 3) {
+//     pagination_endBtn.disabled = true; //nút "End" sẽ bị vô hiệu hóa
+//     prevNext[1].disabled = true; //nút "Next" bị vô hiệu hóa
+//   }
+//   //Nếu currentStep bằng 0, hoặc đang ở trang đầu tiên
+//   else if (currentStep === 0) {
+//     // Nếu ở trang đầu tiên
+//     pagination_startBtn.disabled = true; //nút "Start" sẽ bị vô hiệu hóa
+//     prevNext[0].disabled = true; // nút "Previous" bị vô hiệu hóa
+//   }
+//   //Nếu currentStep không phải là 0 hoặc 3
+//   /*tất cả các nút "Start", "End", "Previous" và "Next" đều được kích hoạt để cho phép người dùng di chuyển qua các trang khác nhau. */
+//   else {
+//     // Tất cả các nút "Bắt đầu", "Kết thúc", "Previous" và "Next" đều được kích hoạt để cho phép người dùng di chuyển qua các trang khác nhau.
+//     pagination_endBtn.disabled = false;
+//     prevNext[1].disabled = false;
+//     pagination_startBtn.disabled = false;
+//     prevNext[0].disabled = false;
+//   }
+// };
 
-// Add event listeners to the number links
-numbers.forEach((number, numIndex) => {
-  number.addEventListener("click", (e) => {
-    e.preventDefault();
-    // Set the current step to the clicked number link
-    currentStep = numIndex;
-    // Remove the "active" class from the previously active number link
-    document.querySelector(".active").classList.remove("active");
-    // Add the "active" class to the clicked number link
-    number.classList.add("active");
-    updateBtn(); // Update the button states
-  });
-});
+// // Thêm event listeners cho các liên kết số trang
+// numbers.forEach((number, numIndex) => {
+//   number.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     //Đặt bước hiện tại thành số trang được nhấp vào
+//     currentStep = numIndex;
+//     // Xóa lớp "active" từ liên kết số trang trước đó
+//     document.querySelector(".active").classList.remove("active");
+//     // Thêm lớp "active" vào liên kết số trang được nhấp vào
+//     number.classList.add("active");
+//     updateBtn(); // Cập nhật trạng thái các nút
+//   });
+// });
 
-// Add event listeners to the "Previous" and "Next" buttons
-prevNext.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    // Increment or decrement the current step based on the button clicked
-    currentStep += e.target.id === "pagination_next" ? 1 : -1;
-    numbers.forEach((number, numIndex) => {
-      // Toggle the "active" class on the number links based on the current step
-      number.classList.toggle("active", numIndex === currentStep);
-      updateBtn(); // Update the button states
-    });
-  });
-});
+// // Thêm các trình nghe sự kiện cho các nút "Previous" và "Next"
+// prevNext.forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     // Tăng hoặc giảm bước hiện tại dựa trên nút được nhấp vào
+//     currentStep += e.target.id === "pagination_next" ? 1 : -1;
+//     numbers.forEach((number, numIndex) => {
+//       /// Chuyển đổi lớp "active" trên các liên kết số trang dựa trên bước hiện tại
+//       number.classList.toggle("active", numIndex === currentStep);
+//       updateBtn(); // Cập nhật trạng thái các nút
+//     });
+//   });
+// });
 
-// Add event listener to the "Start" button
-pagination_startBtn.addEventListener("click", () => {
-  // Remove the "active" class from the previously active number link
-  document.querySelector(".active").classList.remove("active");
-  // Add the "active" class to the first number link
-  numbers[0].classList.add("active");
-  currentStep = 0;
-  updateBtn(); // Update the button states
-  pagination_endBtn.disabled = false;
-  prevNext[1].disabled = false;
-});
+// // Thêm trình nghe sự kiện cho nút "Start"
+// pagination_startBtn.addEventListener("click", () => {
+//   // Xóa lớp "active" từ liên kết số trang trước đó
+//   document.querySelector(".active").classList.remove("active");
+//   // Thêm lớp "active" vào liên kết số trang đầu tiên  numbers[0].classList.add("active");
+//   currentStep = 0;
+//   updateBtn(); // Cập nhật trạng thái các nút
+//   pagination_endBtn.disabled = false;
+//   prevNext[1].disabled = false;
+// });
 
-// Add event listener to the "End" button
-pagination_endBtn.addEventListener("click", () => {
-  // Remove the "active" class from the previously active number link
-  document.querySelector(".active").classList.remove("active");
-  // Add the "active" class to the last number link
-  numbers[3].classList.add("active");
-  currentStep = 4;
-  updateBtn(); // Update the button states
-  pagination_startBtn.disabled = false;
-  prevNext[0].disabled = false;
-});
+// // Thêm trình nghe sự kiện cho nút "Kết thúc"
+// pagination_endBtn.addEventListener("click", () => {
+//   // Xóa lớp "active" từ liên kết số trang trước đó
+//   document.querySelector(".active").classList.remove("active");
+//   // Thêm lớp "active" vào liên kết số trang cuối cùng
+//   numbers[3].classList.add("active");
+//   currentStep = 4;
+//   updateBtn(); // Cập nhật trạng thái các nút
+//   pagination_startBtn.disabled = false;
+//   prevNext[0].disabled = false;
+// });
